ff83f3d093fd62e210ae1b606de403e9cd5ab34c
Abuhadba-Farris <57422694+Farris-Abuhadba@users.noreply.github.com>
2024-08-11 19:07:02 -0400

    Frontend-skeleton

---
+++ Frontend/mwf-dota2-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+//import BuildDisplay from '@/components/BuildDisplay'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">MWF Dota 2 Build Assistant</h1>
+      {/*<BuildDisplay />*/}
+    </main>
+  )
+}
